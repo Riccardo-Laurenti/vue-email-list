241cbe56ed1e2d5b4a39bbecb9748e464c59a017
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-06-27 19:11:49 +0200

    edit script and finish exercise

--- js/script.js
+++ js/script.js
@@ -12,6 +12,37 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 const { createApp } = Vue
 
 createApp({
-
+    data() {
+        return {
+          mailList : [],
+          numberOfMails : 0,
+        }
+      },
+    
+      methods:{
+        addNewEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (result) => {
+               this.mailList.push(result.data.response);
+                    
+            
+              });
+    
+           },
+         
+           generateMailList(numberOfMails){
+            this.numberOfMails = numberOfMails;
+            for(let i=0; i < numberOfMails; i++){
+             this.addNewEmail();
+            }
+           },
+           
+       },
+    
+      created(){
+        this.generateMailList(10);
+        
+       
+      }
 
 }).mount('#my-app')
