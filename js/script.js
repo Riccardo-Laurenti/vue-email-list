c7525522503b04c443961394e1da025f4baf1637
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-06-27 15:03:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+Descrizione:
+
+Attraverso l'apposita API di Boolean :puntare_in_basso::carnagione-2:
+
+https://flynn.boolean.careers/exercises/api/random/mail
+
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+
+const { createApp } = Vue
+
+createApp({
+
+
+}).mount('#my-app')
